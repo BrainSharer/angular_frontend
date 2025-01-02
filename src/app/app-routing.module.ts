ad2bc64e7ead8fbc27cf339214987050626f397b
Edward O'Donnell <eddy.odonnell@gmail.com>
2025-01-02 10:07:09 +0700

    Made a page for polygon creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CollaborationComponent } from './pages/docs/collaboration.component';
 import { FiducialComponent } from './pages/docs/fiducial.component';
 import { SegmentationComponent } from './pages/docs/segmentation.component';
+import { PolygonsComponent } from './pages/docs/polygons.component';
 
 const browseStateModule = () => import('./pages/browse-state/browse-state.module').then(x => x.BrowseStateModule);
 
@@ -15,6 +16,7 @@ const routes: Routes = [
     { path: '', component: HomeComponent  },
     { path: 'collaboration', component: CollaborationComponent  },
     { path: 'fiducial', component: FiducialComponent  },
+    { path: 'polygons', component: PolygonsComponent  },
     { path: 'segmentation', component: SegmentationComponent  },
     // otherwise redirect to home
     { path: '**', redirectTo: ''  }
---
+++ src/app/pages/docs/polygons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-polygons',
+  templateUrl: './polygons.component.html',
+  styleUrls: ['./docs.css']
+})
+export class PolygonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
